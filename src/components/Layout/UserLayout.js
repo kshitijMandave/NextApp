f32ab198e57c9dbb5e3f8d9f024897102e978fe7
kshitijMandave <kshitij.mandwe@gmail.com>
2025-08-06 23:23:20 +0530

    Created one Next App to practice all the concept of Next JS

---
+++ src/components/Layout/UserLayout.js
@@ -0,0 +1,17 @@
+import Footer from "../Common/Footer.js";
+import Header from "../Common/Header.js";
+
+function UserLayout({ children }) {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/*Main */}
+
+      {/*Main */}
+      <Footer />
+    </>
+  );
+}
+
+export default UserLayout;
